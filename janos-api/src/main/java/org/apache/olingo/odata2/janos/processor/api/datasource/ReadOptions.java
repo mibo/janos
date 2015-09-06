96204d883723333af0ef1820c7e81d678839f9be
mibo <mibo@mirb.de>
2015-09-06 09:27:54 +0200

    [Janos#3] Add fields for ReadOptions

--- janos-api/src/main/java/org/apache/olingo/odata2/janos/processor/api/datasource/ReadOptions.java
+++ janos-api/src/main/java/org/apache/olingo/odata2/janos/processor/api/datasource/ReadOptions.java
@@ -1,8 +1,17 @@
 package org.apache.olingo.odata2.janos.processor.api.datasource;
 
+import org.apache.olingo.odata2.api.uri.expression.FilterExpression;
+import org.apache.olingo.odata2.api.uri.expression.OrderByExpression;
+
 /**
+ * Optimized read options.
+ *
  * Created by mibo on 04.09.15.
  */
 public interface ReadOptions {
-
+  FilterExpression getFilter();
+  OrderByExpression getOrderBy();
+  Integer getSkip();
+  String getSkipToken();
+  Integer getTop();
 }
--- janos-core/src/main/java/org/apache/olingo/odata2/janos/processor/core/datasource/GenericReadOptions.java
+++ janos-core/src/main/java/org/apache/olingo/odata2/janos/processor/core/datasource/GenericReadOptions.java
@@ -5,9 +5,16 @@ import org.apache.olingo.odata2.api.uri.expression.OrderByExpression;
 import org.apache.olingo.odata2.janos.processor.api.datasource.ReadOptions;
 
 /**
+ * Optimized read options.
+ *
  * Created by michael on 04.09.15.
  */
 public class GenericReadOptions implements ReadOptions {
+  private FilterExpression filter;
+  private OrderByExpression orderBy;
+  private String skipToken;
+  private Integer skip;
+  private Integer top;
 
   public static ReadOptions none() {
     return new GenericReadOptions();
@@ -15,6 +22,42 @@ public class GenericReadOptions implements ReadOptions {
 
   public static Builder start() {
     return new Builder();
+  }
+
+  public static Builder start(FilterExpression filter) {
+    return new Builder().filter(filter);
+  }
+
+  public static Builder start(OrderByExpression orderBy) {
+    return new Builder().order(orderBy);
+  }
+
+  public static Builder start(String skipToken, Integer skip) {
+    return new Builder().skip(skipToken, skip);
+  }
+
+  public static Builder start(Integer top) {
+    return new Builder().top(top);
+  }
+
+  public FilterExpression getFilter() {
+    return filter;
+  }
+
+  public OrderByExpression getOrderBy() {
+    return orderBy;
+  }
+
+  public Integer getSkip() {
+    return skip;
+  }
+
+  public String getSkipToken() {
+    return skipToken;
+  }
+
+  public Integer getTop() {
+    return top;
   }
 
   public static class Builder {
@@ -25,18 +68,23 @@ public class GenericReadOptions implements ReadOptions {
     }
 
     public Builder filter(FilterExpression filter) {
+      options.filter = filter;
       return this;
     }
 
     public Builder order(OrderByExpression orderBy) {
+      options.orderBy = orderBy;
       return this;
     }
 
     public Builder skip(String skipToken, Integer skip) {
+      options.skipToken = skipToken;
+      options.skip = skip;
       return this;
     }
 
     public Builder top(Integer top) {
+      options.top = top;
       return this;
     }
   }
