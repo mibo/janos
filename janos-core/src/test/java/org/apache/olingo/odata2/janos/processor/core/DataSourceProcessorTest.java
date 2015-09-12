8bff4231ae0db9e2d7cc5381b414500c96778cfc
mibo <mibo@mirb.de>
2015-09-12 16:13:35 +0200

    Enhanced test-utils and added more tests

--- janos-core/src/test/java/org/apache/olingo/odata2/janos/processor/core/DataSourceProcessorTest.java
+++ janos-core/src/test/java/org/apache/olingo/odata2/janos/processor/core/DataSourceProcessorTest.java
@@ -15,16 +15,31 @@
  */
 package org.apache.olingo.odata2.janos.processor.core;
 
+import org.apache.olingo.odata2.api.edm.Edm;
+import org.apache.olingo.odata2.api.edm.EdmEntitySet;
 import org.apache.olingo.odata2.api.exception.ODataException;
+import org.apache.olingo.odata2.api.processor.ODataContext;
+import org.apache.olingo.odata2.api.processor.ODataResponse;
+import org.apache.olingo.odata2.api.uri.PathInfo;
+import org.apache.olingo.odata2.api.uri.info.GetEntitySetUriInfo;
+import org.apache.olingo.odata2.janos.processor.api.data.ReadOptions;
+import org.apache.olingo.odata2.janos.processor.api.data.ReadResult;
 import org.apache.olingo.odata2.janos.processor.api.data.source.DataSource;
 import org.apache.olingo.odata2.janos.processor.api.data.store.DataStoreManager;
 import org.apache.olingo.odata2.janos.processor.api.data.access.ValueAccess;
+import org.apache.olingo.odata2.janos.processor.core.data.access.BeanPropertyAccess;
 import org.apache.olingo.odata2.janos.processor.core.data.source.AnnotationDataSource;
 import org.apache.olingo.odata2.janos.processor.core.data.access.AnnotationValueAccess;
 import org.apache.olingo.odata2.janos.processor.core.model.Building;
+import org.apache.olingo.odata2.janos.processor.core.model.Room;
+import org.apache.olingo.odata2.testutil.helper.JsonHelper;
+import org.apache.olingo.odata2.testutil.helper.StringHelper;
+import org.apache.olingo.odata2.testutil.mock.EdmMock;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
+
+import java.util.*;
 
 /**
  *
@@ -33,7 +48,7 @@ public class DataSourceProcessorTest {
 
   private DataSourceProcessor dataSourceProcessor;
   private DataSource mockedDataSource = Mockito.mock(DataSource.class);
-  private ValueAccess mockedValueAccess = Mockito.mock(ValueAccess.class);
+  private ValueAccess mockedValueAccess = new BeanPropertyAccess();//Mockito.mock(ValueAccess.class);
   private DataStoreManager mockedDataStoreManager = Mockito.mock(DataStoreManager.class);
 
   @Test
@@ -92,5 +107,68 @@ public class DataSourceProcessorTest {
     String url3 = "Rooms?$skiptoken=213";
     String result3 = dataSourceProcessor.percentEncodeNextLink(url3);
     Assert.assertEquals("Rooms", result3);
+  }
+
+  @Test
+  public void readTop() throws Exception {
+    Edm edm = EdmMock.createMockEdm();
+    GetEntitySetUriInfo uriInfo = Mockito.mock(GetEntitySetUriInfo.class);
+    Mockito.when(uriInfo.getTop()).thenReturn(5);
+    EdmEntitySet rooms = EdmMock.getEntitySet(edm, "Rooms");
+    Mockito.when(uriInfo.getTargetEntitySet()).thenReturn(rooms);
+    ODataContext context = Mockito.mock(ODataContext.class);
+    PathInfo pathInfo = Mockito.mock(PathInfo.class);
+    Mockito.when(context.getPathInfo()).thenReturn(pathInfo);
+    dataSourceProcessor.setContext(context);
+
+    Collection<Room> results = createRooms(1, 10);
+    ReadResult<Room> readResult = ReadResult.forResult(results).build();
+    Mockito.when(mockedDataSource.readData(Mockito.any(EdmEntitySet.class), Mockito.any(ReadOptions.class)))
+        .thenReturn((ReadResult) readResult);
+
+    ODataResponse result = dataSourceProcessor.readEntitySet(uriInfo, "application/json");
+    StringHelper.Stream resultStream = StringHelper.toStream(result.getEntityAsStream());
+    List parsedResults = JsonHelper.getResults(resultStream.asString());
+    Assert.assertEquals(5, parsedResults.size());
+  }
+
+  @Test
+  public void optimizedReadTop() throws Exception {
+    Edm edm = EdmMock.createMockEdm();
+    GetEntitySetUriInfo uriInfo = Mockito.mock(GetEntitySetUriInfo.class);
+    Mockito.when(uriInfo.getTop()).thenReturn(5);
+    EdmEntitySet rooms = EdmMock.getEntitySet(edm, "Rooms");
+    Mockito.when(uriInfo.getTargetEntitySet()).thenReturn(rooms);
+    ODataContext context = Mockito.mock(ODataContext.class);
+    PathInfo pathInfo = Mockito.mock(PathInfo.class);
+    Mockito.when(context.getPathInfo()).thenReturn(pathInfo);
+    dataSourceProcessor.setContext(context);
+
+    Collection<Room> results = createRooms(1, 10);
+    ReadResult<Room> readResult = ReadResult.forResult(results).top().build();
+    Mockito.when(mockedDataSource.readData(Mockito.any(EdmEntitySet.class), Mockito.any(ReadOptions.class)))
+        .thenReturn((ReadResult)readResult);
+
+    ODataResponse result = dataSourceProcessor.readEntitySet(uriInfo, "application/json");
+    StringHelper.Stream resultStream = StringHelper.toStream(result.getEntityAsStream());
+    List parsedResults = JsonHelper.getResults(resultStream.asString());
+    Assert.assertEquals(5, parsedResults.size());
+  }
+
+  private List<Room> createRooms(int startId, int amount) {
+    List<Room> results = new ArrayList<>();
+    int max = startId + amount;
+    for (int i = startId; i < max; i++) {
+      results.add(createRoom(i));
+    }
+
+    return results;
+  }
+
+  private Room createRoom(int id) {
+    Room r = new Room(id, "Room with id: " + id);
+    r.setSeats(10 + id);
+    r.setVersion(100 + id);
+    return r;
   }
 }
